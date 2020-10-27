2fee34ae1424df098d92531b2a30ffb94228459c
Wuzhiyin <979446886@qq.com>
2020-10-27 17:31:17 +0800

    step-4 单个任务项的选中状态及样式

--- js/app.js
+++ js/app.js
@@ -21,6 +21,28 @@
 	new Vue({
 		data:{
 			todos
+		},
+		methods: {
+			handleNewTodoKeyDown(e) {
+		        // 0. 注册按下的回车事件
+		        // 1. 获取文本框的内容
+		        // 2. 数据校验
+		        // 3. 添加到 todos 列表
+		        // 4. 清空文本框
+		        // console.log(this.todoText)
+		        const target = e.target
+		        const value = target.value.trim()
+		        if (!value.length) {
+		          return
+		        }
+		        const todos = this.todos
+		        todos.push({
+		          id: todos[todos.length - 1].id + 1,
+		          title: value,
+		          completed: false
+		        })
+		        target.value = ''
+	      	},
 		}
 	}).$mount('#app')
 })();
